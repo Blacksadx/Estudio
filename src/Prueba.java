f3742ac36613069aa335058056e133fb5f1ee714
blacksadx <jhonblacksad@gmail.com>
2022-07-29 19:43:02 -0500

    Cambie el hola mundo a 3

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 
 public class Prueba {
     public static void main (String [] args){
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo 3");
     }
 }
