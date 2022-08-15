08e81ff16934c737368ea9076b2f08fa4789037f
Blacksadx <jhonblacksad@gmail.com>
2022-08-15 14:47:47 -0500

    Cambios para Comenzar

---
+++ Seccion3/src/Type.java
@@ -0,0 +1,18 @@
+public class Type {
+    public static void main (String args[]){
+        int x=2;
+        int y=6;
+        char a='C';
+        char b='a';
+        char c='m';
+        char d='i';
+        char e='l';
+        char f='a';
+        String name="Camila en String";
+
+        System.out.println(x+y);
+        System.out.println(a+b+c+d+e+f);
+        System.out.println(name);
+
+    }
+}
